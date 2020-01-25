323311cac997914e3a7118fad5de709da5b40a9e
DipakRaei <58922864+DipakRaei@users.noreply.github.com>
2020-01-25 12:05:47 +0545

    Add files via upload

---
+++ Vergil_ADC1-master/tuitionTeacherFinder/teacherprofile/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import prof
+
+# Create your views here.
+
+def profile(request):
+	professor = prof.objects.all()
+	return render(request, 'teacherprofile/profile.html',{"dip" : professor})
